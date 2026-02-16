2c75843adfbb75bae9292729394d19c639ce3537
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-02-16 11:32:19 +0600

    Total price

--- totalCost.js
+++ totalCost.js
@@ -1,16 +1,16 @@
 
 const products = [
-    {name: "laptop",    price: 32000},
-    {name: "phone",     price: 7000},
-    {name: "watch",     price: 3000},
-    {name: "tablet",    price: 15000},
-    {name: "headphone", price: 3000},
+    {name: "laptop",    price: 32000,   quantity: 1},
+    {name: "phone",     price: 15000,   quantity: 1},
+    {name: "watch",     price: 250,     quantity: 3},
+    {name: "tablet",    price: 15000,   quantity: 1},
+    {name: "headphone", price: 2000,    quantity: 2},
 ]
 
 function getTotalCost(products){
     let sum = 0;
     for (const product of products) {
-        sum += product.price
+        sum += product.price * product.quantity;
         
     }
     return sum;
